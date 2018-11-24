7a4fbf91f23a86de547cf8fe206ddf176a4aa8fe
Savidude <mysticsavy@gmail.com>
2018-11-25 05:17:56 +0530

    Add script to update Tax Dump

---
+++ bin/update_taxdump.sh
@@ -0,0 +1,20 @@
+echo "Downloading Taxdump"
+curl -OS "ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz"
+currentDir=$PWD
+if [[ -f new_taxdump.tar.gz ]]
+then
+    mkdir new_taxdump
+    cp new_taxdump.tar.gz new_taxdump
+    cd new_taxdump
+    echo "Extracting Taxdump"
+    gunzip new_taxdump.tar.gz
+    tar xvpf new_taxdump.tar
+else
+    echo "BLAST+ to download taxdump"
+fi
+echo "Copying names.dmp to resources"
+cp ${currentDir}/new_taxdump/names.dmp ${currentDir}/../src/main/resources
+echo "Copying nodes.dmp to resources"
+cp ${currentDir}/new_taxdump/nodes.dmp ${currentDir}/../src/main/resources
+rm -rf ${currentDir}/new_taxdump
+echo "Setup complete"
